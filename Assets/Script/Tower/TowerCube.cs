558c12c97e33e05acc9356ac21afb47c270de65c
dai495128713 <49744543+dai495128713@users.noreply.github.com>
2020-06-06 14:54:16 +0800

    添加了Cube的血量设置，通过插值的方式，每秒更新一次

--- Assets/Script/Tower/TowerCube.cs
+++ Assets/Script/Tower/TowerCube.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-
 public class TowerCube : MonoBehaviour
-{   
+{
     [HideInInspector] //被选中的cube的塔
     public GameObject TowerCubeOn;
     //塔属性
@@ -16,41 +15,49 @@ public class TowerCube : MonoBehaviour
     //改变cube颜色
     private Renderer render;
     private Color initColor;
-    private Color changeColor=Color.blue;
+    private Color changeColor = Color.blue;
+    private Color CurrentColor;
     public Color InfectedColor = Color.red;
     public Canvas buildCanves;//创建不同塔的界面
-
     //cube的感染值
-    public int CubeHp = 100;
-    public void ChangeHP(int damage)
+    public float damage = 10;
+    public float CubeHp = 0.0f;
+    //public void ChangeHP(int damage)
+    //{
+    //    CubeHp -= damage;
+    //    if (CubeHp < 0)
+    //    {
+    //        CubeHp = 0;
+    //    }
+    //}
+    private void HpDamage()
     {
-        CubeHp -= damage;
-        if (CubeHp < 0)
-        {
-            CubeHp = 0;
-        }
+        this.CubeHp -= 1;
     }
-
     private void Start()
     {
         render = GetComponent<MeshRenderer>();
         initColor = render.material.color;
         //build.enabled = false;
     }
+    private float time = 0.0f;
     private void Update()
     {
-        //如果被感染了，让脚本失活，cube变色
-        if (CubeHp <= 0)
+        time += Time.deltaTime;
+        if (time >= 1)
         {
-            render.material.color = InfectedColor;
-            if(TowerCubeOn!=null)
-                TowerCubeOn.GetComponent<Tower>().enabled = false;
+            time = 0;
+            HpSetting(damage);
         }
-        else
+        //如果被感染了，让脚本失活，cube变色
+        if (CubeHp <= 100)
         {
-            render.material.color = initColor;
+            Vector3 nowRGB = new Vector3(render.material.color.r, render.material.color.g, render.material.color.b);
+            Vector3 newRGB = new Vector3(CubeHp / 100.0f, (100 - CubeHp) / 100.0f, 0);
+            render.material.color = new Color(Vector3.Lerp(nowRGB, newRGB, Time.deltaTime).x, Vector3.Lerp(nowRGB, newRGB, Time.deltaTime).y, Vector3.Lerp(nowRGB, newRGB, Time.deltaTime).z);
             if (TowerCubeOn != null)
-                TowerCubeOn.GetComponent<Tower>().enabled = true ;
+            //TowerCubeOn.GetComponent<Tower>().enabled = false;
+                Destroy(TowerCubeOn);
         }
     }
     public void BuildTower(TowerData tower)
@@ -58,7 +65,7 @@ public class TowerCube : MonoBehaviour
         this.towerdata = tower;
         isUpgrade = false;
         TowerCubeOn = GameObject.Instantiate(towerdata.TowerPrefab, this.transform.position, Quaternion.identity);
-        
+
         //UI
         //GameObject effect = GameObject.Instantiate(buildeffect, transform.position, Quaternion.identity);
         //Destroy(effect, 1);
@@ -80,6 +87,20 @@ public class TowerCube : MonoBehaviour
         //GameObject effect = GameObject.Instantiate(buildeffect, transform.position, Quaternion.identity);
         //Destroy(effect, 1);
 
+    }
+    public void HpSetting(float num)
+    {
+        if (CubeHp < 0)
+        {
+            CubeHp = 0;
+            return;
+        }
+        if (CubeHp > 100)
+        {
+            CubeHp = 100;
+            return;
+        }
+        CubeHp += num;
     }
     private void OnMouseEnter()
     {
