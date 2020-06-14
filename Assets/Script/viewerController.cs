8c3e393da8db26f82325c43262b545b4e81d7787
chase <1554658086@qq.com>
2020-06-14 20:18:11 +0800

    新地图

---
+++ Assets/Script/viewerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class viewerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Update is called once per frame
+
+    public float speed = 4;
+    public float mousespeed = 60;
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");//x
+        float v = Input.GetAxis("Vertical");//z
+        float mouse = Input.GetAxis("Mouse ScrollWheel");
+
+        transform.Translate(new Vector3(h*speed, mouse*mousespeed, v*speed) * Time.deltaTime, Space.World);
+    }
+}
