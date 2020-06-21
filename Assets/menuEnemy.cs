3516ec9f71dc2a15dc5c2edd02c17a7e138551f7
Z3budah <mirah.z3budah@foxmail.com>
2020-06-22 02:41:53 +0800

    在Menu界面搭景

---
+++ Assets/menuEnemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuEnemy : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+
+    }
+
+
+}
